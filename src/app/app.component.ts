82e0f74c13301b2dd77bd88d7e7016a9e0d0dbca
Flavio Carvalho <flaviocarvalho.brz@gmail.com>
2024-02-14 01:19:22 -0300

    Lesson 1 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ClientsComponent,
+    GalleryComponent,
+    NavComponent,
+    PricingComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
